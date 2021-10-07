07df22bf68a31f773ea29b8e93067a3940a4a2fb
Adeleke Ayinde <lekea4@gmail.com>
2021-10-07 17:49:43 +0100

    Implement the location base class

---
+++ EventCenter.Domain/Base/LocationBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventCenter.Domain.Base
+{
+    public class LocationBase
+    {
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
