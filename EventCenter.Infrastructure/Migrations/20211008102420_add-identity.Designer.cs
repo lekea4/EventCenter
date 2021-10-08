4db7fc7f319fe922a7fe3a02f5302f115da9253e
Adeleke Ayinde <lekea4@gmail.com>
2021-10-08 11:41:32 +0100

    Create a new migration based on Identity and also changes made the entites

--- EventCenter.Infrastructure/Migrations/20211008102420_add-identity.Designer.cs
+++ EventCenter.Infrastructure/Migrations/20211008102420_add-identity.Designer.cs
@@ -4,14 +4,16 @@ using EventCenter.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EventCenter.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20211008102420_add-identity")]
+    partial class addidentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
